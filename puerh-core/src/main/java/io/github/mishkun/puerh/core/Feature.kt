77728777bfee10b7880c4e58b63ed48c393f491f
Mikhail Levcheko <mishkun.lev@gmail.com>
2020-03-27 19:02:54 +0300

    Rename Cmd to Eff
    Here Cmd is not really the same as in The Elm Architecture. So to avoid
    confusion, I decided to name it Eff

--- puerh-core/src/main/java/io/github/mishkun/puerh/core/Feature.kt
+++ puerh-core/src/main/java/io/github/mishkun/puerh/core/Feature.kt
@@ -9,49 +9,49 @@ interface Cancelable {
     fun cancel()
 }
 
-interface Feature<Msg : Any, Model : Any, Cmd : Any> {
+interface Feature<Msg : Any, Model : Any, Eff : Any> {
     val currentState: Model
     fun accept(msg: Msg)
     fun listenState(listener: (model: Model) -> Unit): Cancelable
-    fun listenEffect(listener: (cmd: Cmd) -> Unit): Cancelable
+    fun listenEffect(listener: (eff: Eff) -> Unit): Cancelable
 }
 
-class SyncFeature<Msg : Any, Model : Any, Cmd : Any>(
+class SyncFeature<Msg : Any, Model : Any, Eff : Any>(
     initialState: Model,
-    private val reducer: (Msg, Model) -> Pair<Model, Set<Cmd>>
-) : Feature<Msg, Model, Cmd> {
+    private val reducer: (Msg, Model) -> Pair<Model, Set<Eff>>
+) : Feature<Msg, Model, Eff> {
     override var currentState: Model = initialState
         private set
     private val stateListeners = mutableListOf<(state: Model) -> Unit>()
-    private val cmdListeners = mutableListOf<(cmd: Cmd) -> Unit>()
+    private val effListeners = mutableListOf<(eff: Eff) -> Unit>()
 
     override fun accept(msg: Msg) {
         val (newState, commands) = reducer(msg, currentState)
         currentState = newState
         stateListeners.notifyAll(newState)
         commands.forEach { command ->
-            cmdListeners.notifyAll(command)
+            effListeners.notifyAll(command)
         }
     }
 
     override fun listenState(listener: (state: Model) -> Unit): Cancelable =
         stateListeners.addListenerAndMakeCancelable(listener)
 
-    override fun listenEffect(listener: (cmd: Cmd) -> Unit): Cancelable =
-        cmdListeners.addListenerAndMakeCancelable(listener)
+    override fun listenEffect(listener: (eff: Eff) -> Unit): Cancelable =
+        effListeners.addListenerAndMakeCancelable(listener)
 }
 
-class AndroidHandlerFeature<Msg : Any, Model : Any, Cmd : Any>(
+class AndroidHandlerFeature<Msg : Any, Model : Any, Eff : Any>(
     initialState: Model,
-    private val reducer: (Msg, Model) -> Pair<Model, Set<Cmd>>
-) : Feature<Msg, Model, Cmd> {
+    private val reducer: (Msg, Model) -> Pair<Model, Set<Eff>>
+) : Feature<Msg, Model, Eff> {
     private val reduceThread = HandlerThread("reducer thread").apply {
         start()
     }
     private val reduceHandler = Handler(reduceThread.looper)
     private val callerThreadHandler = Handler(Looper.myLooper())
     private val stateListeners = mutableListOf<(state: Model) -> Unit>()
-    private val cmdListeners = mutableListOf<(cmd: Cmd) -> Unit>()
+    private val effListeners = mutableListOf<(eff: Eff) -> Unit>()
 
     override var currentState: Model = initialState
         private set
@@ -63,8 +63,8 @@ class AndroidHandlerFeature<Msg : Any, Model : Any, Cmd : Any>(
             callerThreadHandler.post {
                 currentState = newState
                 stateListeners.notifyAll(newState)
-                commands.forEach { cmd ->
-                    cmdListeners.notifyAll(cmd)
+                commands.forEach { eff ->
+                    effListeners.notifyAll(eff)
                 }
             }
         }
@@ -73,8 +73,8 @@ class AndroidHandlerFeature<Msg : Any, Model : Any, Cmd : Any>(
     override fun listenState(listener: (state: Model) -> Unit): Cancelable =
         stateListeners.addListenerAndMakeCancelable(listener)
 
-    override fun listenEffect(listener: (cmd: Cmd) -> Unit): Cancelable =
-        cmdListeners.addListenerAndMakeCancelable(listener)
+    override fun listenEffect(listener: (eff: Eff) -> Unit): Cancelable =
+        effListeners.addListenerAndMakeCancelable(listener)
 }
 
 private fun <T> MutableList<(T) -> Unit>.addListenerAndMakeCancelable(listener: (T) -> Unit): Cancelable {
